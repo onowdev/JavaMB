b6e36aa375ac49767def3fc5c45bfe5d681c7b41
onowdev <onowdev@gmail.com>
2022-11-30 21:46:26 +0700

    penjumlahan

--- src/com/onowdev/babsatu/TipeData.java
+++ src/com/onowdev/babsatu/TipeData.java
@@ -5,9 +5,12 @@ public class TipeData {
         short a = 90;
         int b = 980000;
         long c = 900000L;
+        long d = a +b +c ;
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+
+        System.out.println("Hasil Penjumlahan adalah : " + d);
     }
 }
