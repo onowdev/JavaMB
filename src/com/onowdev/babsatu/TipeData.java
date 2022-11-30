8c20319cd8905f01d4b6d214213db1d12e02d115
onowdev <onowdev@gmail.com>
2022-11-30 21:08:46 +0700

    tipedata

---
+++ src/com/onowdev/babsatu/TipeData.java
@@ -0,0 +1,13 @@
+package com.onowdev.babsatu;
+
+public class TipeData {
+    public static void main(String[] args) {
+        short a = 90;
+        int b = 980000;
+        long c = 900000L;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
