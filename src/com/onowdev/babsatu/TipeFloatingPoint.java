6da229141207c682a571da998f0c542bcb967b88
onowdev <onowdev@gmail.com>
2022-12-02 08:41:21 +0700

    penjumlahan

---
+++ src/com/onowdev/babsatu/TipeFloatingPoint.java
@@ -0,0 +1,8 @@
+package com.onowdev.babsatu;
+
+public class TipeFloatingPoint {
+    public static void main(String[] args) {
+        // tipe data yang digunakan untuk merepresentasikan Nilai2 angka desimal / pecahan
+
+    }
+}
